2228bb6f14403dd852938cdb3e3c341b9569879a
Michael <midpeter444@gmail.com>
2011-04-29 10:12:50 -0400

    Initial commit of Agile.Web.Development.with.Rails depot application

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Depot::Application.config.secret_token = 'ee9467dd5a886dd0afc4b388deb8aa39b6d449be6dadc43f1d62426648967292c02b6fe540ac10e66d55cd1a97be1e2badfd9af5556042393abda0f8d9d5dacf'
