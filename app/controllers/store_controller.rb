2e7a1e52f41af7439793413d090a222e4c6d0971
Michael <midpeter444@gmail.com>
2011-05-23 19:58:47 -0400

    Completed ch. 8, including exercises.

--- app/controllers/store_controller.rb
+++ app/controllers/store_controller.rb
@@ -1,6 +1,14 @@
 class StoreController < ApplicationController
   def index
     @products = Product.all
+    @currtime = current_time
+  end
+
+
+  # helper method
+  #~TODO: probably should go into a Helper class ...
+  def current_time
+    Time.now.strftime("%d-%b-%Y %I:%M%p")
   end
 
 end
--- test/functional/products_controller_test.rb
+++ test/functional/products_controller_test.rb
@@ -2,6 +2,7 @@ require 'test_helper'
 
 class ProductsControllerTest < ActionController::TestCase
   setup do
+    # this is a call to the fixtures reference (:one is a yaml key)
     @product = products(:one)
     @update = {
       :title => "Loreum Ipsum",
@@ -15,11 +16,21 @@ class ProductsControllerTest < ActionController::TestCase
     get :index
     assert_response :success
     assert_not_nil assigns(:products)
+    assert_select ".list_description dl dt", products(:one).title
+    assert_select ".list_description dl dt", products(:two).title
+    assert_select ".list_description dl dt", products(:ruby).title
+    assert_select ".list_actions a", "Show"
+    assert_select ".list_actions a", "Edit"
+    assert_select ".list_actions a", "Destroy"
+    assert_select "a[href='#{new_product_path}']", 'New Product'
   end
 
   test "should get new" do
     get :new
     assert_response :success
+    assert_select "a[href='#{products_path}']", "Back"
+    assert_select "label[for='product_title']", "Title"
+    assert_select "#product_title", 1
   end
 
   test "should create product" do
@@ -28,11 +39,15 @@ class ProductsControllerTest < ActionController::TestCase
     end
 
     assert_redirected_to product_path(assigns(:product))
+    #~what assert_select's could we do here? - what file test against?
   end
 
   test "should show product" do
     get :show, :id => @product.to_param
     assert_response :success
+    assert_select "p b", "Title:"
+    assert_select "a[href='#{edit_product_path(@product)}']", 1
+    assert_select "#banner", /Pragmatic Bookshelf/
   end
 
   test "should get edit" do
